4a787323c3ed3d17f372f1e8ccec0dba1e5efcb9
Krauchman <rauan.omarov@nu.edu.kz>
2019-05-06 19:58:21 +0600

    Removed contest info route without contest_pk

--- contest/urls.py
+++ contest/urls.py
@@ -5,7 +5,6 @@ from problem import views as problem_views
 from submission import views as submission_views
 
 urlpatterns = [
-    path('', views.info, name='contest-info'),
     path('<int:contest_pk>/', views.info, name='contest-info'),
     path('<int:contest_pk>/ranking', views.ranking, name='contest-ranking'),
     path('<int:contest_pk>/problem/<int:problem_pk>', problem_views.problem_page, name='problem-page'),
